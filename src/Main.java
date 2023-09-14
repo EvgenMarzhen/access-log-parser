a91f013139da95ce397501d22c53b550faa4d902
Evgeny Marinichev <evg5743@yandex.ru>
2023-09-14 17:07:14 +0300

    Rollback master branch

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Ввод двух чисел, на основании которых будет выполняться операция
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
-        System.out.println("Расчет:");
-
-        //Выводим сумму
-        int sum = firstNumber + secondNumber;
-        System.out.println("Сумма двух чисел: " + sum);
-
-        //Выводим разность
-        int diff = firstNumber - secondNumber;
-        System.out.println("Разность двух чисел " + diff);
-
-        //Выводим произведение
-        int multiply = firstNumber * secondNumber;
-        System.out.println("Произведение двух чисел: " + multiply);
-
-        //Выводим частное
-        double quotient = (double) firstNumber / secondNumber;
-        System.out.println("Частное двух чисел: " + quotient);
+        System.out.println("Введите текст и нажмите <Enter>");
+        //Сканер длины строки(Курсовой проект)
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
