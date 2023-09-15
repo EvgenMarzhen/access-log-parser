e8de9d26833079f53b9a740478ff6d6d597034e5
Evgeny Marinichev <evg5743@yandex.ru>
2023-09-15 15:12:59 +0300

    test commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите текст и нажмите <Enter>");
-        //Сканер длины строки(Курсовой проект) 111
+        //Сканер длины строки(Курсовой проект)
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
     }
