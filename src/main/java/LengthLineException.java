ba11cc80e9ac7927693746dace96e87525fc5708
Evgeny Marinichev <evg5743@yandex.ru>
2026-02-19 17:33:34 +0300

    added new Exception and added new funcional for working file:  - Counting length line  - print max length line  - min length line

---
+++ src/main/java/LengthLineException.java
@@ -0,0 +1,19 @@
+public class LengthLineException extends RuntimeException{
+    public LengthLineException() {
+        super();
+    }
+
+    public LengthLineException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,58 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
         int countCorPath = 0;
+        String path; //= "D:\\JAVALearn\\StepUp\\AccessLogParser\\access.log";
 
         while (true) {
-            String path = new Scanner(System.in).nextLine();
+            System.out.println("Укажите путь к файлу");
+            path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
+
             if(isDirectory) {
                 System.out.println("Указана папка, а не файл");
-            } else if (!fileExists){
-                System.out.println("Файл не найден");
             }
-            else {
+
+            if (!fileExists){
+                System.out.println("Файл не найден");
+            } else {
                 countCorPath++;
                 System.out.printf("Путь указан верно %d раз(-a)\n", countCorPath);
+            }
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+
+                String line;
+                int countLine = 0;
+                int maxLength = 0;
+                int minLength = Integer.MAX_VALUE;
+
+                while ((line = reader.readLine()) != null) {
+                    if(line.length() > 1024) throw new LengthLineException("Строка не должна быть длиннее 1024 символов");
+
+                    countLine++;
+
+                    if(line.length() > maxLength) {
+                        maxLength = line.length();
+                    }
+
+                    if(line.length() < minLength) {
+                        minLength = line.length();
+                    }
+                }
+
+                System.out.println("Общее количество строк в файле: " + countLine);
+                System.out.println("Самая длинная строка в файле: " + maxLength);
+                System.out.println("Самая короткая строка в файле: " + minLength);
+
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
             }
         }
     }
