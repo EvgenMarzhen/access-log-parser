a75611abc8c26a27c0085d4d57a0c890e41123dd
Evgeny Marinichev <evg5743@yandex.ru>
2026-02-20 19:01:11 +0300

    removed the code for printing the max and min line length

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,26 +30,12 @@ public class Main {
 
                 String line;
                 int countLine = 0;
-                int maxLength = 0;
-                int minLength = Integer.MAX_VALUE;
 
                 while ((line = reader.readLine()) != null) {
                     if(line.length() > 1024) throw new LengthLineException("Строка не должна быть длиннее 1024 символов");
+                    
 
-                    countLine++;
-
-                    if(line.length() > maxLength) {
-                        maxLength = line.length();
-                    }
-
-                    if(line.length() < minLength) {
-                        minLength = line.length();
-                    }
                 }
-
-                System.out.println("Общее количество строк в файле: " + countLine);
-                System.out.println("Самая длинная строка в файле: " + maxLength);
-                System.out.println("Самая короткая строка в файле: " + minLength);
 
             } catch (IOException ioException) {
                 ioException.printStackTrace();
