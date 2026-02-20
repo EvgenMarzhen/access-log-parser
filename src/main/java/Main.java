b3f1f6e07725a4324d97aabe47bdf1b1d9ed164b
Evgeny Marinichev <evg5743@yandex.ru>
2026-02-20 20:27:47 +0300

    split the log by components and displayed the share of YandexBot and GoogleBot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,11 +13,11 @@ public class Main {
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
 
-            if(isDirectory) {
+            if (isDirectory) {
                 System.out.println("Указана папка, а не файл");
             }
 
-            if (!fileExists){
+            if (!fileExists) {
                 System.out.println("Файл не найден");
             } else {
                 countCorPath++;
@@ -29,13 +29,67 @@ public class Main {
                 BufferedReader reader = new BufferedReader(fileReader);
 
                 String line;
-                int countLine = 0;
+                int countReq = 0;
+                int countYandexBot = 0;
+                int countGoogleBot = 0;
 
                 while ((line = reader.readLine()) != null) {
-                    if(line.length() > 1024) throw new LengthLineException("Строка не должна быть длиннее 1024 символов");
-                    
+                    if (line.length() > 1024) {
+                        throw new LengthLineException("Строка не должна быть длиннее 1024 символов");
+                    }
 
+                    String[] logComponents = line.split(" ");
+
+                    String ip = logComponents[0];
+                    String someProperty1 = logComponents[1];
+                    String someProperty2 = logComponents[2];
+                    String dateTime = logComponents[3] + " " + logComponents[4];
+                    String methodAndPath = logComponents[5] + " " + logComponents[6] + " " + logComponents[7];
+                    String httpStatus = logComponents[8];
+                    String sizeResponse = logComponents[9];
+                    String someProperty3 = logComponents[10];
+                    StringBuilder userAgent = new StringBuilder();
+
+                    for (int i = 11; i < logComponents.length; i++) {
+                        userAgent.append(logComponents[i]).append(" ");
+                    }
+
+                    String fragment = "";
+
+                    String[] parts = userAgent.toString().split(";");
+                    if (parts.length >= 2) {
+                        fragment = parts[1];
+                    }
+                    String cleanFragment = fragment.trim();
+
+                    String[] programs = cleanFragment.split("/");
+                    String program = "";
+
+                    if (programs.length > 0) {
+                        program = programs[0];
+                    }
+
+                    countReq++;
+
+                    if (program.equalsIgnoreCase("YandexBot")) {
+                        countYandexBot++;
+                    }
+
+                    if (program.equalsIgnoreCase("GoogleBot")) {
+                        countGoogleBot++;
+                    }
                 }
+
+                System.out.println("Общее число запросов: " + countReq);
+
+                if (countReq > 0) {
+                    System.out.printf("Доля запросов от YandexBot: %.2f%%\n", (double) countYandexBot / countReq * 100);
+                }
+
+                if (countReq > 0) {
+                    System.out.printf("Доля запросов от GoogleBot: %.2f%%\n", (double) countGoogleBot / countReq * 100);
+                }
+
 
             } catch (IOException ioException) {
                 ioException.printStackTrace();
