e759afeb2c236fb2454d8c5d004a81fcbf6386db
Evgeny Marinichev <evg5743@yandex.ru>
2026-02-23 21:03:49 +0300

    - Added Statistics.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
 import exceptions.LengthLineException;
 import logs.LogEntry;
-import logs.UserAgent;
+import monitoring.Statistics;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int countCorPath = 0;
-        String path = "D:\\JAVALearn\\StepUp\\AccessLogParser\\access.log";
+        String path;
 
-//        while (true) {
-//            System.out.println("Укажите путь к файлу");
-//            path = new Scanner(System.in).nextLine();
+        Statistics statistics = new Statistics();
+
+        while (true) {
+            System.out.println("Укажите путь к файлу");
+            path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
@@ -39,56 +42,16 @@ public class Main {
                     }
 
                     LogEntry logEntry = new LogEntry(line);
-//                    System.out.println(logEntry);
-//                    System.out.println(logEntry.getDateTime());
+                    statistics.addEntry(logEntry);
 
-                    UserAgent userAgent = new UserAgent(logEntry.getUserAgent());
-                    System.out.print(userAgent.getBrowser() + " ");
-                    System.out.println(userAgent.getOsType());
-//                    userAgent.printProperty(line);
-
-
-
-//                    String fragment = "";
-//
-//                    String[] parts = userAgent.toString().split(";");
-//                    if (parts.length >= 2) {
-//                        fragment = parts[1];
-//                    }
-//                    String cleanFragment = fragment.trim();
-//
-//                    String[] programs = cleanFragment.split("/");
-//                    String program = "";
-//
-//                    if (programs.length > 0) {
-//                        program = programs[0];
-//                    }
-//
-//                    countReq++;
-//
-//                    if (program.equalsIgnoreCase("YandexBot")) {
-//                        countYandexBot++;
-//                    }
-//
-//                    if (program.equalsIgnoreCase("GoogleBot")) {
-//                        countGoogleBot++;
-//                    }
                 }
 
-//                System.out.println("Общее число запросов: " + countReq);
-//
-//                if (countReq > 0) {
-//                    System.out.printf("Доля запросов от YandexBot: %.2f%%\n", (double) countYandexBot / countReq * 100);
-//                }
-//
-//                if (countReq > 0) {
-//                    System.out.printf("Доля запросов от GoogleBot: %.2f%%\n", (double) countGoogleBot / countReq * 100);
-//                }
-
+                System.out.println("Общий трафик: " + statistics.getTotalTraffic());
+                System.out.printf("Средний трафик в час: %.2f\n", statistics.getTrafficRate());
 
             } catch (IOException ioException) {
                 ioException.printStackTrace();
             }
-//        }
+        }
     }
 }
--- src/main/java/monitoring/Statistics.java
+++ src/main/java/monitoring/Statistics.java
@@ -1,4 +1,40 @@
 package monitoring;
 
+import logs.LogEntry;
+import lombok.Getter;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Getter
 public class Statistics {
+    private int totalTraffic;
+    private LocalDateTime minTime = LocalDateTime.MAX;
+    private LocalDateTime maxTime = LocalDateTime.MIN;
+
+    public Statistics() {
+    }
+
+    public void addEntry(LogEntry logEntry) {
+        if (logEntry.getSizeResponse() != null) {
+            totalTraffic += logEntry.getSizeResponse();
+        }
+
+        if (logEntry.getDateTime().isAfter(getMaxTime())) {
+            maxTime = logEntry.getDateTime();
+        }
+
+        if (logEntry.getDateTime().isBefore(getMinTime())) {
+            minTime = logEntry.getDateTime();
+        }
+    }
+
+    public double getTrafficRate() {
+        if (minTime.equals(maxTime)) {
+            return 0.0;
+        }
+        long diffSeconds = Duration.between(minTime, maxTime).getSeconds();
+        double diffHours = diffSeconds / 3600.0;
+        return totalTraffic / diffHours;
+    }
 }
